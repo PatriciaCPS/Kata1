6934a905c4d679d2ebf9f0f1753a0b2812eb3279
Patricia <Patricia@DESKTOP-I85JAJP>
2021-12-25 17:28:24 +0000

    Kata1_version3 completa

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,12 +1,13 @@
 package kata1;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
   private final String name;
-  private final Date birthdate;
-
-    public Person(String name, Date birthdate) {
+  private final LocalDate birthdate;
+  private final LocalDate today = LocalDate.parse("2021-12-25"); 
+    public Person(String name, LocalDate birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -15,14 +16,15 @@ public class Person {
         return name;
     }
 
-    public Date getBirthdate() {
+    public LocalDate getBirthdate() {
         return birthdate;
     }
   
     public int getAge(){
-    
-        return (int)((new Date().getTime()- birthdate.getTime())/31536000000L); 
-    
+        Period period = Period.between(birthdate,today);
+        int edad = (int) period.getYears();
+        return edad;
+        
     }
   
   
